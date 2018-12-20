0025015f5a03cfccc337cc692c7bfb848c93006e
fpereira <fernando.pereira@poptrade.com.br>
2018-12-20 16:51:07 -0200

    two-way-binding, passando dados do componente pai para filho

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -13,15 +13,34 @@ export class PainelComponent implements OnInit {
   public Frases: Frase[] = frases
   public instrucao: string = "Traduza a frase"
   public resposta: string
+
+  public rodada: number = 0
+  public rodadaFrase: Frase
+
+  public count_progresso: number = 0
   
-  constructor() { }
+  constructor() {
+    this.rodadaFrase = this.Frases[this.rodada]
+   }
 
   ngOnInit() {
   }
 
   public atualizaResposta(resposta: Event): void {
-    this.resposta = (<HTMLInputElement>resposta.target).value
-    console.log(resposta)
+    this.resposta = (<HTMLInputElement>resposta.target).value    
   }  
+
+  public verificarResposta(): void {
+
+    if (this.rodadaFrase.frasePtBr.toLowerCase() == this.resposta.toLowerCase()) {      
+      alert("A tradução está correta!")
+      this.rodada++      
+      this.rodadaFrase = this.Frases[this.rodada]
+      this.count_progresso = this.count_progresso + (100 / this.Frases.length)
+      console.log(this.count_progresso)
+    } else {
+      alert("A tradução está errada!")
+    }    
+  }
 
 }
