27c8a0f37a85507f2771c37d4a4e84e6970186ec
fpereira <fernando.pereira@poptrade.com.br>
2019-02-07 15:56:21 -0200

    Coloquei mais umas coisas (sistema de pontuação)

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, EventEmitter, Output, OnDestroy } from '@angular/cor
 
 import { Frase } from '../shared/frase.model';
 import { frases } from './frases-mock';
+import { THROW_IF_NOT_FOUND } from '@angular/core/src/di/injector';
 
 declare function JSExterno(): any;
 
@@ -21,14 +22,19 @@ export class PainelComponent implements OnInit {
 
   public count_progresso: number = 0
 
-  public tentativas: number = 3
+  public tentativas: number = frases.length -1
+  public acertos: number = 0
 
   public fraseCorreta: string
+
+  public podeProsseguir: boolean = true
+  public podeVerificar: boolean = false
 
   @Output() public encerrarJogo: EventEmitter<string> = new EventEmitter()
   
   constructor() {
-    this.atualizaRodada()
+    this.rodadaFrase = this.Frases[this.rodada]
+    this.resposta = ''    
   }
 
   ngOnInit() {
@@ -39,41 +45,77 @@ export class PainelComponent implements OnInit {
 
   public atualizaResposta(resposta: Event): void {
     this.resposta = (<HTMLInputElement>resposta.target).value  
+    this.trocaParaVerificar()
   }  
 
   public verificarResposta(): void {
 
-    if (this.rodadaFrase.frasePtBr.toLowerCase() == this.resposta.toLowerCase()) {      
+    if (this.rodadaFrase.frasePtBr.toLowerCase().trim() == this.resposta.toLowerCase().trim()) {        
 
-      this.rodada++   
+      this.acertos += 1      
 
-      this.count_progresso = this.count_progresso + (100 / this.Frases.length)   
+      this.atualizaRodada()
 
-      if (this.rodada === this.Frases.length)
+      if (this.acertos === this.Frases.length)
       {
-        this.encerrarJogo.emit('vitoria')
-        alert('Parabéns, você completou todas as traduções!!!')
+        this.vitoria()
       }
-      else
-      {
-        this.atualizaRodada()
-      }
-    
+
     } else {
 
       this.tentativas--
 
       this.fraseCorreta = this.rodadaFrase.frasePtBr
 
+      this.trocaParaProsseguir()
+
       if (this.tentativas === -1) {
-        this.encerrarJogo.emit('derrota')
-        alert('Você perdeu todas as tentativas')
+        this.fimDeJogo()
       }
-    }
+    }    
+      
   }
 
   public atualizaRodada(): void {
-    this.rodadaFrase = this.Frases[this.rodada]
-    this.resposta = ''
+
+    this.rodada++   
+    this.count_progresso = this.count_progresso + (100 / this.Frases.length)   
+    console.log(`tentativas = ${this.tentativas}`)
+    console.log(`rodada = ${this.rodada}`)
+    console.log(`total frases = ${this.Frases.length}`)
+
+    if (this.rodada == this.Frases.length) {
+      this.fimDeJogo()
+    } else {
+
+      this.rodadaFrase = this.Frases[this.rodada]
+      this.resposta = ''    
+      this.fraseCorreta = ''
+    }    
   }  
+
+  private trocaParaProsseguir(): void {
+    this.podeProsseguir = false
+    this.podeVerificar = true
+  }
+
+  private trocaParaVerificar(): void {
+    this.podeProsseguir = true
+    this.podeVerificar = false
+  }  
+
+  private fimDeJogo(): void {
+    if (this.tentativas === -1) {
+      this.encerrarJogo.emit('derrota')
+        alert('Todas as tentativas foram esgotadas... fim de jogo XS')      
+    } else if (this.acertos > 0) {
+      this.encerrarJogo.emit('derrota')
+      alert(`Você fez ${this.acertos} pontos, mas não conseguiu completar o desafio. Tente novamente ;)`)      
+    }
+  }
+
+  private vitoria(): void {
+    this.encerrarJogo.emit('vitoria')
+    alert('Mandou bem! Completou todas as traduções corretamente. Você ganhou! xD')    
+  }
 }
